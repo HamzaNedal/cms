1677de8f602804fb50f3a1056b82876ec2bf1de4
HamzaNedal <hamza.05926170@gmail.com>
2021-08-15 21:49:18 +0300

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -58,8 +58,8 @@ class User extends Authenticatable implements MustVerifyEmail
     {
         return $this->status == 1 ?  __('Active') : __('Inactive');
     }
-    public function setPasswordAttribute($password)
-    {   
-        //$this->attributes['password'] = bcrypt($password);
-    }
+//     public function setPasswordAttribute($password)
+//     {   
+//         //$this->attributes['password'] = bcrypt($password);
+//     }
 }
